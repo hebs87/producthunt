3169c7692f2908bde00275b8b2c64ee783da508c
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-30 16:08:27 +0000

    Added accounts and products to settings.py INSTALLED_APPS

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'products',
     'django_cleanup.apps.CleanupConfig',
 ]
 
