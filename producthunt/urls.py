f8a4612e3d11af28c512ae7ebbd530b3aad1d25d
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-31 10:57:40 +0000

    Added products and MEDIA_ROOT urls to root settings.py

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -17,9 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from products import views
 from accounts import urls as accounts_urls
+from products import urls as products_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('accounts/', include(accounts_urls)),
+    path('products/', include(products_urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
