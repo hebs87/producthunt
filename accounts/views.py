1c4667584b936206cb03ce202cca9f04f7d37047
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-31 08:41:20 +0000

    Created basic login view to render login.html

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def register(request):
     '''
-    Renders register.htm and allows user to register
+    Renders register.html and allows user to register
     '''
     return(render, 'register.html')
+
+
+def login(request):
+    '''
+    Renders login.html and allows user to register
+    '''
+    return(render, 'login.html')
+
+
