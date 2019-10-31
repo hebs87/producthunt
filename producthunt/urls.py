6e88aa05f9e7c91a2dc3f57e038539cbdb7a2476
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-31 11:15:47 +0000

    Created create.html and config root urls.py

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from products import views
 from accounts import urls as accounts_urls
 from products import urls as products_urls
