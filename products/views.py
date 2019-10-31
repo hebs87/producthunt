59b65cc1d4a1570654c7343bd596e372a1503feb
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-31 11:12:31 +0000

    Created basic create view to render create.html

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -7,3 +8,11 @@ def home(request):
     Returns home page
     '''
     return render(request, 'index.html')
+
+
+@login_required
+def create(request):
+    '''
+    Allows user to add a product
+    '''
+    return render(request, 'create.html')
