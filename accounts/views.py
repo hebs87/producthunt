54da8afdf8d0be8d164f6d2836e369ece33f99a2
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-31 09:23:10 +0000

    Updated register view to load register_form

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 def register(request):
     '''
     Renders register.html and allows user to register
     '''
-    return render(request, 'register.html')
+    register_form = UserRegistrationForm()
+
+    context = {
+        'register_form': register_form,
+    }
+
+    return render(request, 'register.html', context)
 
 
 def login(request):
