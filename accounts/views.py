bed9b657b7ed0333a7f305623c3b5a1ed0fb859a
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-31 08:40:33 +0000

    Created basic register view to render register.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def register(request):
+    '''
+    Renders register.htm and allows user to register
+    '''
+    return(render, 'register.html')
