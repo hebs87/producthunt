fcc12b9557d9d8a787f07849255ea1db6c9e5085
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-31 09:45:09 +0000

    Amended register view to verify username and passwords

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 from .forms import UserLoginForm, UserRegistrationForm
@@ -8,7 +8,28 @@ def register(request):
     '''
     Renders register.html and allows user to register
     '''
-    register_form = UserRegistrationForm()
+    if request.method == "POST":
+        register_form = UserRegistrationForm(request.POST)
+
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                # Check to see if username already exists
+                user = User.objects.get(username=request.POST["username"])
+                return render(request, 'register.html',
+                    {'register_form': register_form})
+            except User.DoesNotExist:
+                # If username doesn't exist, create user
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password2"]
+                )
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'register.html',
+                    {'register_form': register_form})
+    else:
+        register_form = UserRegistrationForm()
 
     context = {
         'register_form': register_form,
